658ab7e3885f7af1b7e972d579f192537c1556cd
Renars Dilevka <renars.dilevka@tangentlabs.co.uk>
2012-11-09 15:18:56 +0000

    Adds actual file not symlink

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,13 @@
-.git/hooks/pre-commit.sh
+#!/usr/bin/env bash
+
+# Please symlink this to your ,git/hooks/ directory
+
+# Check JS
+FILES_PATTERN='\.(js|coffee)(\..+)?$'
+FORBIDDEN='console.log'
+git diff --cached --name-only | \
+	grep -E $FILES_PATTERN | \
+	GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "console.log" references in your javascript. Please remove them before commiting' && exit 1
+
+exit 0
+
